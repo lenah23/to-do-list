9d8d114ae179c0e45ba7230f0197579a258c0301
lenah23 <lenulstep@gmail.com>
2024-10-13 13:07:14 +0400

    app ts test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import App from './App';
+import { store } from './store/store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders TodoCard', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  const headingElement = screen.getByText(/todos/i);
+  expect(headingElement).toBeInTheDocument();
 });
