eea96a22fd33ea34bf917e9539b3fb952d223463
lenah23 <lenulstep@gmail.com>
2024-10-13 12:42:08 +0400

    configured jest and added unit tests for  custom input

---
+++ src/tests/CustomInput.test.tsx
@@ -0,0 +1,52 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { CustomInput } from '../components';
+
+describe('CustomInput Component', () => {
+  const mockOnChange = jest.fn();
+  const mockHandleKeyPress = jest.fn();
+
+  it('renders with the correct placeholder', () => {
+    render(
+      <CustomInput
+        onChange={mockOnChange}
+        placeholder='Enter text'
+        handleKeyPress={mockHandleKeyPress}
+        value=''
+      />
+    );
+    expect(screen.getByPlaceholderText('Enter text')).toBeInTheDocument();
+  });
+
+  it('calls onChange handler when typing', () => {
+    render(
+      <CustomInput
+        onChange={mockOnChange}
+        placeholder='Enter text'
+        handleKeyPress={mockHandleKeyPress}
+        value=''
+      />
+    );
+    const input = screen.getByPlaceholderText('Enter text');
+    fireEvent.change(input, { target: { value: 'New value' } });
+    expect(mockOnChange).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls handleKeyPress handler when a key is pressed', () => {
+    render(
+      <CustomInput
+        onChange={mockOnChange}
+        placeholder='Enter text'
+        handleKeyPress={mockHandleKeyPress}
+        value=''
+      />
+    );
+    const input = screen.getByPlaceholderText('Enter text');
+
+    input.focus();
+    fireEvent.keyPress(input, { key: 'Enter', code: 'Enter', charCode: 13 });
+
+    console.log(mockHandleKeyPress.mock.calls); 
+
+    expect(mockHandleKeyPress).toHaveBeenCalledTimes(1);
+  });
+});
